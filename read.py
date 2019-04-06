ce23aa3b9e757085d9f9714d38e6adff9337e8ef
Erika Wu <ironance@hotmail.com>
2019-04-07 00:06:44 +0800

    Add word count

--- read.py
+++ read.py
@@ -4,10 +4,10 @@ with open('reviews.txt', 'r') as f:
     for line in f:
         data.append(line)
         count += 1
-        if count % 1000 == 0:
-            print(len(data))
-
+        # if count % 1000 == 0:
+        #     print(len(data))
 print('All reviews are read. There are', len(data), 'reviews in total.')
+
 
 #count the average length of the reviews
 total_len = 0
@@ -31,3 +31,33 @@ for d in data:
         good.append(d)
 print('There are', len(good), 'reviews which contains "good".')
 print(good[0])
+
+
+
+#count the number of each word
+wc = {} #word_count
+for d in data:
+    words = d.split()
+    for word in words:
+        if word in wc:
+            wc[word] += 1
+        else:
+            wc[word] = 1
+
+for word in wc:
+    if wc[word] > 1000000:
+        print(word, wc[word])
+
+while True:
+    word = input('Which word do you want to search: ')
+    if word == 'q':
+        print('Thanks for using!')
+        break
+    if word in wc:
+        print('The number of "', word, '" in the reviews is', wc[word])
+    else:
+        print('The word is not exist.')
+
+
+
+
